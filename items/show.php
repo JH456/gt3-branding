e0f81a132aa874aacf75b5cbe395562be9436012
Jim Harris <jharris9456@gmail.com>
2018-02-16 14:53:35 -0500

    Kill the ugly borders again, and rearrange format of document page

--- items/show.php
+++ items/show.php
@@ -1,52 +1,87 @@
-<?php echo head(array('title' => metadata('item', array('Dublin Core', 'Title')),'bodyclass' => 'items show')); ?>
 
-<h1><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h1>
+<?php queue_css_url('https://www.w3schools.com/w3css/4/w3.css') ?>
+<?php echo head(array('title' => metadata('item', array('Dublin Core', 'Title')),'bodyclass' => 'items show w3Page')); ?>
 
-<div id="primary">
+<?php
+$calledPlugins = array();
 
-    <?php if ((get_theme_option('Item FileGallery') == 0) && metadata('item', 'has files')): ?>
-    <?php echo files_for_item(array('imageSize' => 'fullsize')); ?>
-    <?php endif; ?>
+function fire_specific_plugin_hook($pluginName, $hookName, $args = array())
+{
+    // Get the specific hook.
+    $pluginBroker = get_plugin_broker();
+    $hookNameSpecific = $pluginBroker->getHook($pluginName, $hookName);
 
-    <?php echo all_element_texts('item'); ?>
+    // Return null if the specific hook doesn't exist.
+    if (!$hookNameSpecific) {
+        return null;
+    }
 
-    <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
+    foreach ($hookNameSpecific as $cb) {
+        call_user_func($cb, $args);
+    }
+}
+?>
 
-</div><!-- end primary -->
+<div class='w3-row'>
+    <h1><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h1>
+</div>
 
-<aside id="sidebar">
+<!-- The following prints a list of all tags associated with the item -->
+<?php if (metadata('item', 'has tags')): ?>
+<div class='w3-row' id="item-tags">
+    <div class="element-text"><?php echo tag_string('item'); ?></div>
+    <br>
+</div>
+<?php endif;?>
 
-    <!-- The following returns all of the files associated with an item. -->
-    <?php if ((get_theme_option('Item FileGallery') == 1) && metadata('item', 'has files')): ?>
-    <div id="itemfiles" class="element">
-        <h2><?php echo __('Files'); ?></h2>
-        <?php echo item_image_gallery(); ?>
-    </div>
-    <?php endif; ?>
+<div class='w3-row' style='display: flex; justify-content: space-between;'>
+    <section id='primary'>
+        <?php if ((get_theme_option('Item FileGallery') == 0) && metadata('item', 'has files')): ?>
+        <?php fire_specific_plugin_hook('DocsViewer', 'public_items_show', array('view' => $this, 'item' => $item)) ?>
+        <?php endif; ?>
+    </section>
+    <aside id="sidebar">
+        <section>
+            <?php echo all_element_texts('item'); ?>
+            <?php fire_specific_plugin_hook('Scripto', 'public_items_show', array('view' => $this, 'item' => $item)) ?>
+        </section>
+        <!-- The following returns all of the files associated with an item. -->
+        <?php if ((get_theme_option('Item FileGallery') == 1) && metadata('item', 'has files')): ?>
+        <div id="itemfiles" class="element">
+            <h2><?php echo __('Files'); ?></h2>
+            <?php echo item_image_gallery(); ?>
+        </div>
+        <?php endif; ?>
 
-    <!-- If the item belongs to a collection, the following creates a link to that collection. -->
-    <?php if (metadata('item', 'Collection Name')): ?>
-    <div id="collection" class="element">
-        <h2><?php echo __('Collection'); ?></h2>
-        <div class="element-text"><p><?php echo link_to_collection_for_item(); ?></p></div>
-    </div>
-    <?php endif; ?>
+        <!-- If the item belongs to a collection, the following creates a link to that collection. -->
+        <?php if (metadata('item', 'Collection Name')): ?>
+        <div id="collection" class="element">
+            <h2><?php echo __('Collection'); ?></h2>
+            <div class="element-text"><p><?php echo link_to_collection_for_item(); ?></p></div>
+        </div>
+        <?php endif; ?>
+    </aside>
+</div>
 
-    <!-- The following prints a list of all tags associated with the item -->
-    <?php if (metadata('item', 'has tags')): ?>
-    <div id="item-tags" class="element">
-        <h2><?php echo __('Tags'); ?></h2>
-        <div class="element-text"><?php echo tag_string('item'); ?></div>
-    </div>
-    <?php endif;?>
+<div class='w3-row'>
+    <section>
+        <?php fire_specific_plugin_hook('ItemRelations', 'public_items_show', array('view' => $this, 'item' => $item)) ?>
+    </section>
+</div>
 
+<div class='w3-row'>
     <!-- The following prints a citation for this item. -->
-    <div id="item-citation" class="element">
+    <section id="item-citation" class="element">
         <h2><?php echo __('Citation'); ?></h2>
         <div class="element-text"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
-    </div>
+    </section>
+</div>
 
-</aside>
+<section>
+    <?php /*fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item));*/ ?>
+    <?php fire_specific_plugin_hook('Commenting', 'public_items_show', array('view' => $this, 'item' => $item)) ?>
+</section>
+
 
 <ul class="item-pagination navigation">
     <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
